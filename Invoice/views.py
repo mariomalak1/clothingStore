4ffcb2dcf7b753991ea3b0c5561df980e46659d4
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-04-18 04:06:35 +0200

    add delete confirmation code for orders in cart

--- Invoice/views.py
+++ Invoice/views.py
@@ -7,6 +7,9 @@ def all_in_cart(request, cart_code):
     context = {
         "code": cart.cart_code,
         "total": cart.total_price(),
-        "orders_in_cart": cart.order_set.all()
+        "orders_in_cart": cart.order_set.all(),
+        "delete_label": "Delete Order Confirmation",
+        "delete_message": "you sure that you want to delete this order ?",
+
     }
     return render(request, "Invoice/all_in_cart.html", context)
