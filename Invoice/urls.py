940aca343572ec865f3edae93002f068c3ca3baa
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-06-29 01:44:33 +0300

    start in create invoice pdf, html, css, not compelete

--- Invoice/urls.py
+++ Invoice/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path("create_invoice/", views.create_invoice, name="create_invoice"),
 ]
 
--- Invoice/views.py
+++ Invoice/views.py
@@ -1,2 +1,6 @@
 from django.shortcuts import render
-# Create your views here.
+
+# create views here.
+
+def create_invoice(request):
+    return render(request, "Invoice/invoice.html")
