f00991f698a1d38cb97b367847d0717e12ba3e53
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-07-02 01:41:55 +0300

    start in invoice template

--- Invoice/urls.py
+++ Invoice/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("create_invoice/", views.create_invoice, name="create_invoice"),
+    path("create_invoice/<str:cart_code>", views.create_invoice, name="create_invoice"),
+
 ]
 
--- Invoice/views.py
+++ Invoice/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from django.http import Http404
+from Invoice.models import Cart
 # create views here.
 
-def create_invoice(request):
-    return render(request, "Invoice/invoice.html")
+def create_invoice(request, cart_code):
+    cart_ = Cart.objects.filter(cart_code=cart_code).first()
+    if cart_:
+        context = {
+            "cart":cart_,
+        }
+        return render(request, "Invoice/invoice.html", context)
+    else:
+        return Http404()
