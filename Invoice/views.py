ec4d38d4f6964268c20414ae273f4b6d8cb01a45
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-07-02 19:46:54 +0300

    create invoice inforce

--- Invoice/views.py
+++ Invoice/views.py
@@ -1,12 +1,12 @@
+import pdfkit
+import io
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponse
+from django.template.loader import render_to_string
+
 from Invoice.models import Cart
 from Seller.models import Site_User
-# from weasyprint import HTML
 # create views here.
-
-def create_invoice_pdf(request):
-    return HttpResponse()
 
 
 def create_invoice(request, cart_code):
@@ -17,6 +17,29 @@ def create_invoice(request, cart_code):
             "cart":cart_,
             "current_user":current_user,
         }
-        return render(request, "Invoice/invoice.html", context)
+        template = render(request, "Invoice/invoice.html", context)
+        html_content = template.content.decode('utf-8')
+        path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+        config = pdfkit.configuration(wkhtmltopdf=path_wkhtmltopdf)
+        options = {
+            'dpi': 365,
+            'page-size': 'A4',
+            'margin-top': '0in',
+            'margin-right': '0in',
+            'margin-bottom': '0in',
+            'margin-left': '0in',
+            'custom-header': [
+                ('Accept-Encoding', 'gzip')
+            ],
+            'no-outline': None,
+            "enable-local-file-access": "",
+        }
+        pdf = pdfkit.from_string(html_content, options=options, configuration=config)
+        # Create the HttpResponse object with the PDF content
+        response = HttpResponse(pdf, content_type='application/pdf')
+
+        # Set the Content-Disposition header to force a download
+        response['Content-Disposition'] = f'attachment; filename="{cart_.cart_code}.pdf"'
+        return response
     else:
         return Http404()
