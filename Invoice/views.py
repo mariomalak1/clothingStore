c0fbd9315b3e5ae4f4538e43f8860f85eeb9d469
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-07-03 14:07:16 +0300

    adding logo on pdf, put logo in logo dir

--- Invoice/views.py
+++ Invoice/views.py
@@ -1,8 +1,12 @@
 import pdfkit
 import io
+import base64
+import os
+from project.settings import BASE_DIR
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponse
 from django.template.loader import render_to_string
+from django.templatetags.static import static
 
 from Invoice.models import Cart
 from Seller.models import Site_User
@@ -13,10 +17,17 @@ def create_invoice(request, cart_code):
     cart_ = Cart.objects.filter(cart_code=cart_code).first()
     current_user = get_object_or_404(Site_User, id=request.user.id)
     if cart_:
+        # to put logo on
+        logo_path = os.path.join(BASE_DIR, 'static/Logo', 'MarioLogo.png')
+        with open(logo_path, 'rb') as logo_file:
+            logo_data = base64.b64encode(logo_file.read()).decode('utf-8')
+
         context = {
             "cart":cart_,
             "current_user":current_user,
+            "logo_data":logo_data,
         }
+
         template = render(request, "Invoice/invoice.html", context)
         html_content = template.content.decode('utf-8')
         path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
