6ce62aaaa0ca6800669b867b2d287f13c7659834
mario malak <93185168+mariomalak1@users.noreply.github.com>
2023-07-04 14:28:50 +0300

    create first model for notifiaction

---
+++ notification/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Notification(models.Model):
+    title = models.CharField(max_length=100)
+    message = models.TextField(null=True)
+    link = models.CharField(max_length=500, null=True)
